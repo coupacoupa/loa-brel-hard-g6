a4f7b458d61ad4f6e8ddf13007bdcad51c7f7098
coupacoupa <44894563+coupacoupa@users.noreply.github.com>
2023-05-02 19:01:24 +0800

    config: revert timing to original

--- src/components/particles/constants/game.constant.ts
+++ src/components/particles/constants/game.constant.ts
@@ -1,5 +1,5 @@
-export const YELLOW_APPEAR_TILL_DROP_BUFFER = 0;
-export const TILE_RECOVERY = 10;
+export const YELLOW_APPEAR_TILL_DROP_BUFFER = 13;
+export const TILE_RECOVERY = 100;
 export const BLUE_METEOR_INTERVAL = 80;
 export const YELLOW_DAMAGE = 3;
 export const BLUE_DAMAGE = 1;
