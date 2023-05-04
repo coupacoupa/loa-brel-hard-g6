7304ca0db40b969a4fcfdb20b61ad4c2d7fed94d
coupacoupa <44894563+coupacoupa@users.noreply.github.com>
2023-05-04 19:04:56 +0800

    feat: added indicator for number of blue selected

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,19 @@ import { useGameInteraction } from "./components/particles/context/game-interact
 import { Tile } from "./components/particles/types/game";
 
 function App() {
-  const { tiles, resetGame, start, dropYellowMeteor } = useGameInteraction();
+  const {
+    tiles,
+    resetGame,
+    start,
+    dropYellowMeteor,
+    blueInput,
+    nextBlueCount,
+  } = useGameInteraction();
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-10 space-y-24 px-8">
-      <div className="flex flex-row gap-4">
-        <div className="h-full w-full grid-rows-3 gap-2">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-2 h-full w-full grid-rows-3 gap-2">
           <button onClick={() => dropYellowMeteor(2)}>YELLOW TOP</button>
           <div className="m-20 grid -rotate-45 grid-cols-3 gap-2">
             {Object.values(tiles).map((tile: Tile, i) => (
@@ -25,6 +32,21 @@ function App() {
           <div className="grid grid-cols-2 gap-4">
             <button onClick={() => start()}>START</button>
             <button onClick={() => resetGame()}>RESET</button>
+          </div>
+          <h1 className="mb-4 mt-6 text-center text-base font-medium text-gray-900 sm:text-3xl">
+            Blue Input
+          </h1>
+          <div className="grid w-3/4 grid-cols-4">
+            {Array.from({ length: nextBlueCount }, (_, i) => (
+              <div
+                key={i}
+                className={`flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 text-xs text-gray-700 ${
+                  i < blueInput.length ? "border-2 border-blue-600" : undefined
+                }`}
+              >
+                {i + 1}
+              </div>
+            ))}
           </div>
         </div>
       </div>
